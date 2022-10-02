975abf8036b37568ebc2d7452a5c9957d2afcdaf
vikas kumar <108992398+vikaskr775246@users.noreply.github.com>
2022-10-02 18:04:12 +0530

    update about and formbox
    Adding about image rotate effect js and form adding smtp.js library this time is working mailbox

---
+++ script.js
@@ -0,0 +1,32 @@
+function querySelector(value, parent = document) {
+    return parent.querySelector(`${value}`);
+};
+//about event start
+const full_Name = querySelector('#full_Name');
+const email = querySelector('#email');
+const subject = querySelector('#subject');
+const massage = querySelector('#massage');
+const about_mouse = () => {
+    const nav_1 = querySelector('#nav_1');
+    const about_img = querySelector('#about-img');
+    about_img.onmouseenter = () => {
+        about_img.children[0].classList.add('active');
+    };
+    about_img.onmouseleave = () => {
+        about_img.children[0].classList.remove('active');
+    };
+}
+about_mouse();
+//about event end
+
+const SubmitEvent = () => {
+    Email.send({
+        SecureToken: '9acd081f-bada-471c-a40c-167d8311c6ba',
+        To: "vikashkr775246@gmail.com",
+        From: "vikashkr775246@gmail.com",
+        Subject: subject.value,
+        Body: 'Name: ' + full_Name.value + '<br> Email: ' + email.value + '<br> Massage: ' + massage.value
+    }).then(
+        msg => alert('Message Successfully Sent')
+    ).catch(error => console.log(error));
+}
